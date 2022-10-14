e1c5348e9866a55ef69bc808702bbb16690f77f1
Medmly20208 <moulaymohamed856@gmail.com>
2022-10-14 22:40:21 +0100

    code enhancement / UI improvement

--- script.js
+++ script.js
@@ -1,76 +1,85 @@
-let u = document.getElementById("but")
-let f = document.querySelector(".up")
-let bo = document.getElementById("box")
-let d;
-let ne;
-async function get(z) {
-    let u = await fetch("https://www.breakingbadapi.com/api/characters")
-    let r = await u.json()
-    let arr = []
-    let index, ty;
-    for (let i = 0; i < r.length; i++) {
-        arr.push(r[i].name)
+let searchButton = document.getElementById("searchButton");
+let f = document.querySelector(".up");
+let container = document.querySelector(".container");
+let box = document.getElementById("box");
+let CharacterValuesResponse;
+async function getCharacterData(CharacterName) {
+  let ApiResponse = await fetch(
+    "https://www.breakingbadapi.com/api/characters"
+  );
+  let JsonAPIResponse = await ApiResponse.json();
+  let arr = [];
+  let index, NoCharcterFoundBoxContent;
+  for (let i = 0; i < JsonAPIResponse.length; i++) {
+    arr.push(JsonAPIResponse[i].name);
+  }
+  if (arr.indexOf(CharacterName) === -1) {
+    if (document.getElementById("box") != null) {
+      document.getElementById("box").remove();
     }
-    if (arr.indexOf(z) === -1) {
-        if (document.getElementById('box') != null) {
-            document.getElementById('box').remove()
-        }
 
-        ty = document.createElement("div")
-        ty.id = "box"
-        ty.innerHTML = "<p><span>Oops</span> <br> No character with this Name</p>"
-        ty.style.height = "100px"
-        ty.style.paddingTop = "40px"
-        ty.style.fontFamily = "Arial, Helvetica, sans-serif";
+    NoCharcterFoundBoxContent = document.createElement("div");
+    NoCharcterFoundBoxContent.id = "box";
+    NoCharcterFoundBoxContent.innerHTML =
+      "<p><span>Oops</span> <br> No character with this Name</p>";
+    NoCharcterFoundBoxContent.style.height = "100px";
+    NoCharcterFoundBoxContent.style.paddingTop = "40px";
 
+    container.appendChild(NoCharcterFoundBoxContent);
+  } else {
+    index = arr.indexOf(CharacterName);
 
-        f.appendChild(ty)
-
-    } else {
-        index = arr.indexOf(z)
-
-        ne = `
-           <img src="${r[index].img}"  alt="nothing" width=300 height=300><br>
+    CharacterValuesResponse = `
+           <img src="${
+             JsonAPIResponse[index].img
+           }"  alt="nothing" width=300 height=300><br>
            <br>
-           <p id=para><span class=y>Name</span> : <span class=repon>${r[index].name}</span><br>
+           <p id=para><span class=y>Name</span> : <span class=repon>${
+             JsonAPIResponse[index].name
+           }</span><br>
            <br>
-           <span class=y>Birthday</span> : <span class=repon>${r[index].birthday}</span><br>
+           <span class=y>Birthday</span> : <span class=repon>${
+             JsonAPIResponse[index].birthday
+           }</span><br>
            <br>
-           <span class=y>Occupation</span> : <span class=repon>${r[index].occupation.join("/")}</span><br>
+           <span class=y>Occupation</span> : <span class=repon>${JsonAPIResponse[
+             index
+           ].occupation.join("/")}</span><br>
            <br>
-           <span class=y>Status</span> : <span class=repon>${r[index].status}</span><br>
+           <span class=y>Status</span> : <span class=repon>${
+             JsonAPIResponse[index].status
+           }</span><br>
            <br>
-           <span class=y>Appearance</span> : <span class=repon>${r[index].appearance}</span><br>
+           <span class=y>Appearance</span> : <span class=repon>${
+             JsonAPIResponse[index].appearance
+           }</span><br>
            <br>
-           <span class=y>Nickname</span> : <span class=repon>${r[index].nickname}</span><br>
+           <span class=y>Nickname</span> : <span class=repon>${
+             JsonAPIResponse[index].nickname
+           }</span><br>
            <br>
-           <span class=y>Portrayed</span> : <span class=repon>${r[index].portrayed}</span><br>
+           <span class=y>Portrayed</span> : <span class=repon>${
+             JsonAPIResponse[index].portrayed
+           }</span><br>
         
         
          </p>
-        `
-        if (document.getElementById('box') != null) {
-            document.getElementById('box').remove()
-        }
-
-        ty = document.createElement("div")
-        ty.id = "box"
-        ty.innerHTML = ne
-        f.appendChild(ty)
-
-
+        `;
+    if (document.getElementById("box") != null) {
+      document.getElementById("box").remove();
     }
 
-
+    NoCharcterFoundBoxContent = document.createElement("div");
+    NoCharcterFoundBoxContent.id = "box";
+    NoCharcterFoundBoxContent.innerHTML = CharacterValuesResponse;
+    container.appendChild(NoCharcterFoundBoxContent);
+  }
 }
 
-u.onclick = function() {
-    d = document.getElementById("loe").value
-    get(d)
-}
+searchButton.onclick = function () {
+  getCharacterData(document.getElementById("inputField").value);
+};
 
-
-
-bo.onclick = function() {
-    bo.style.color = "white"
-}
+box.onclick = function () {
+  box.style.color = "white";
+};
